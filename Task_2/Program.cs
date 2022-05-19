5cd1136e2159027a5cb62c9d05ad15b16b56d4e2
AntonBelov <asbelov1907@gmail.com>
2022-05-19 23:54:26 +0300

    2 и 3  задача сделана

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int summ(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(summ(number));
---
+++ Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+
+
+    int[] array = new int[8];
+    Random randomaiser = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randomaiser.Next(1,100);
+}
+
+
+
+PrintArray(array);
