9231a53f73c302c269d4ba72b06802bca620fc1c
AntonBelov <asbelov1907@gmail.com>
2022-05-19 22:45:05 +0300

    1 задача сделана

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberB = int.Parse(Console.ReadLine());
+int result = NatureDegree(numberA, numberB);
+Console.WriteLine(result);
+
+
+
+
+
+int NatureDegree(int numA, int numB)
+{
+    int Degree = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        Degree = Degree*numberA;
+        
+    }
+    return Degree;
+}
+
+
